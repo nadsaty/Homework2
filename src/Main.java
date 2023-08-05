42e4a5e03497f18cd9c79d02dc14d916d925deed
Усман <nadsatyyy@gmail.com>
2023-08-06 02:41:10 +0300

    Домашка 1 Задание 3

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,13 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        //Вычитание (Задача 3)
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
         }
     }
