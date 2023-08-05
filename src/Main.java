f78947b0f39a49f3b2724ec153a7cf32edd3ca88
Усман <nadsatyyy@gmail.com>
2023-08-06 02:40:50 +0300

    Домашка 1 Задание 2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,13 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        //Сумма (Задача 2)
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
         }
     }
